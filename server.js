824a1a08214f5b07d8390e6c87b5c8936314e78b
qiushi <qiushi@qiushideMacBook-Pro.local>
2016-07-18 22:49:57 +0800

    静态服务器

--- server.js
+++ server.js
@@ -7,11 +7,16 @@ var PORT = 3010;
 var server = http.createServer(function(request, response){
     // 得到文件路径
     var pathname = url.parse(request.url).pathname;
+    console.log('pathname = ',pathname)
+    if (pathname === '/') pathname = pathname + 'index.html'
     // 设置资源文件路径
     var realPath = "assets" + pathname;
     // 判断资源文件是否存在
     console.log(realPath)
     fs.exists(realPath, function(exists) {
+        // 区分加载类型
+        var ext = path.extname(realPath);
+        ext = ext ? ext.slice(1) : 'unknown';
         if (!exists) {
             response.writeHead(404, {
                 'Content-Type':'text/plain'
@@ -19,7 +24,7 @@ var server = http.createServer(function(request, response){
             response.write('THis request URL '+ pathname + "was not found on this server");
             response.end();
         } else {
-            fs.readFile(readFile, 'binary', function(err, file) {
+            fs.readFile(realPath, 'binary', function(err, file) {
                 if (err) {
                     response.writeHead(500, {
                         'Content-Type': 'text/plain'
@@ -27,12 +32,13 @@ var server = http.createServer(function(request, response){
 
                     response.end(err);
                 } else {
+                    var contentType = mime[ext] || "text/plain";
                     response.writeHead(200, {
-                        'Content-Type': 'text/html'
+                        'Content-Type': contentType,
+                        'Connection': 'keep-alive',
+                        'Transfer-Encoding': chunked
                     });
-
                     response.write(file, "binary");
-
                     response.end();
                 }
             })
